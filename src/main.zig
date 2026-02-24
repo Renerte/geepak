14fc53306d846b648f82a17172938d4ba3ac3f63
Renerte <renerte@renerte.net>
2026-02-24 13:23:41 +0100

    Fix handling missing arguments

--- src/main.zig
+++ src/main.zig
@@ -14,15 +14,14 @@ pub fn main() !void {
     const cwd = std.fs.cwd();
     var archive: ?std.fs.File = null;
     var directory: ?std.fs.Dir = null;
-    var firstArg: []u8 = undefined;
+    var firstArg: ?[]u8 = null;
     var mode = Mode.none;
     var argsIter = try std.process.argsWithAllocator(allocator);
     if (!argsIter.skip()) return;
     while (argsIter.next()) |arg| {
         switch (mode) {
             .none => {
-                firstArg = try allocator.alloc(u8, arg.len);
-                @memcpy(firstArg, arg);
+                firstArg = try allocator.dupe(u8, arg);
                 const stat = cwd.statFile(arg) catch |err| switch (err) {
                     error.IsDir => {
                         directory = try cwd.openDir(arg, .{ .iterate = true });
@@ -51,22 +50,27 @@ pub fn main() !void {
         }
     }
     argsIter.deinit();
-    if (archive == null) {
-        const archivePath = try fileFromDirectory(allocator, firstArg);
-        defer allocator.free(archivePath);
-        archive = try cwd.createFile(archivePath, .{});
-    } else if (directory == null) {
-        directory = try cwd.makeOpenPath(directoryFromFile(firstArg), .{});
-    }
-    allocator.free(firstArg);
     switch (mode) {
-        .none => std.log.warn("Usage: geepak <source archive/directory> [target file/directory]", .{}),
-        .pack => try geepak.packArchive(allocator, directory.?, archive.?),
-        .unpack => geepak.unpackArchive(allocator, archive.?, directory.?) catch |e| {
-            std.log.err("Error unpacking the archive:\n\t{}", .{e});
-            return e;
+        .none => std.log.info("Usage: geepak <source archive/directory> [target file/directory]", .{}),
+        .pack => {
+            if (archive == null) {
+                const archivePath = try fileFromDirectory(allocator, firstArg.?);
+                defer allocator.free(archivePath);
+                archive = try cwd.createFile(archivePath, .{});
+            }
+            try geepak.packArchive(allocator, directory.?, archive.?);
+        },
+        .unpack => {
+            if (directory == null) {
+                directory = try cwd.makeOpenPath(directoryFromFile(firstArg.?), .{});
+            }
+            geepak.unpackArchive(allocator, archive.?, directory.?) catch |e| {
+                std.log.err("Error unpacking the archive:\n\t{}", .{e});
+                return e;
+            };
         },
     }
+    if (firstArg != null) allocator.free(firstArg.?);
     if (builtin.mode == .Debug) {
         _ = gpa.deinit();
     }
